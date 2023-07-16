041b20cd9a17c79623144bb851d273fac861e1c9
Mindgibber <mindgibber@sapo.pt>
2023-07-16 13:13:16 +0100

    Day2 initial commit

--- 2015/src/Day02.zig
+++ 2015/src/Day02.zig
@@ -1,3 +1,13 @@
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const input = try std.fs.cwd().openFile("input/Day02", .{});
+    defer input.close();
+    var buf_input = std.io.bufferedReader(input.reader());
+    var buffer: [4096]u8 = undefined;
+    var buffered_reader = buf_input.reader();
+    var total: u32 = 0;
+    while (try buffered_reader.readUntilDelimiterOrEof(&buffer, '\n')) |line| {
+        for (std.mem.splitScalar(u8, buf, 'x')) || {}
+    }
+}
